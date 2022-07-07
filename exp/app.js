f7f09f3f7ac2a96cc6b258e674b40d498e2d08b1
Miloserdie <89547280+Miloserdie@users.noreply.github.com>
2022-07-07 15:44:51 +0300

    1

--- exp/app.js
+++ exp/app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	if (req.url === '/home') {
+		res.end("Home");
+	} else {
+		next();
+	}
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (req, res, next) {
+	if (url.parse(req.url).pathname === '/forbidden' && checkAccess(req)) {
+		 res.send('Access approved');
+	} else {
+		 res.send('Access denied');
+	}
 });
+
+function checkAccess(req) {
+	return url.parse(req.url, true).query.secret === 'true';
+}
+ 
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 module.exports = app;
